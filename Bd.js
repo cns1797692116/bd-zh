48236bbb0424fbc625878f9e902865acd5a15558
cns1797692116 <81603206+cns1797692116@users.noreply.github.com>
2023-05-23 09:01:43 +0800

    Update Bd.js

--- Bd.js
+++ Bd.js
@@ -21,6 +21,7 @@ function tunnelTLSFinished() {
     _writeHttpHeader()
     httpStatus = HTTP_STATUS_CONNECTED
     return true
+console.log("Tls握手成功")
 }
 //从代理服务器读取到数据回调
 function tunnelDidRead(data) {
@@ -29,8 +30,8 @@ function tunnelDidRead(data) {
         //Assume success here
         console.log("http handshake success")
         httpStatus = HTTP_STATUS_FORWARDING
-        $tunnel.established($session)//可以进行数据转发
-        return null//不将读取到的数据转发到客户端
+        $tunnel.established($session)//
+        return null//
     } else if (httpStatus == HTTP_STATUS_FORWARDING) {
         return data
     }
@@ -39,11 +40,13 @@ function tunnelDidRead(data) {
 function tunnelDidWrite() {
     if (httpStatus == HTTP_STATUS_CONNECTED) {
         console.log("write http head success")
+ console.log("写请求头成功")
         httpStatus = HTTP_STATUS_WAITRESPONSE
         $tunnel.readTo($session, "\x0D\x0A\x0D\x0A")//读取远端数据直到出现\r\n\r\n
         return false //中断wirte callback
     }
     return true
+console.log("发送至代理服务器成功")
 }
 //会话关闭
 function tunnelDidClose() {
@@ -55,6 +58,7 @@ function _writeHttpHeader() {
     let conHost = $session.conHost
     let conPort = $session.conPort
 
-    var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: 157.0.148.53:443\r\nConnection: keep-alive\r\nUser-Agent: User-Agent: BaiduBoxApp/13.33.0 iPhone; CPU iPhone OS 16_4 like Mac OS X\r\nX-T5-Auth: 683556433\r\n\r\n`
+    var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: 157.0.148.53:443\r\nConnection: keep-alive\r\nUser-Agent: BaiduBoxApp/13.33.0 iPhone; CPU iPhone OS 16_4 like Mac OS X\r\nX-T5-Auth: 123456789\r\n\r\n`
+console.log("执行完成释放内存")
     $tunnel.write($session, header)
 }
